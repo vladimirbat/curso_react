c31fc78e30e95fbbf629ebfe16c1fa9166dc8c79
Vladimir Bataller <vbatalle@everis.com>
2020-10-26 17:01:49 +0100

    insertar persona

--- a02-redux-inicial/src/components/Principal.tsx
+++ a02-redux-inicial/src/components/Principal.tsx
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import '../estilos.css';
 import { TablaPersonas } from './TablaPersonas';
 // import {Persona } from '../js/matriz_personas';
-import {createBorrarAction, store} from '../js/store-creator';
+import {createBorrarAction, createInsertarAction, store} from '../js/store-creator';
+import { InsertarPersona } from './InsertarPersona';
+import { Persona } from '../js/matriz_personas';
 // -------------- CONTENEDOR ----------------
 // -- Trabaja directamente con redux
 export class Principal extends Component<{},{}>{
     // constructor(props:{}){
-    //     super(props);
+        //     super(props);
         
-    // }
-
+        // }
+        
+    insertar (p:Persona): void {
+        store.dispatch(createInsertarAction(p));
+    };
     borrarPersona(dni:number) {
         store.dispatch(createBorrarAction(dni));
         console.log('personas',store.getState().personas.length)
@@ -20,11 +25,12 @@ export class Principal extends Component<{},{}>{
         const {personas} = store.getState();
         return  (
             <div>
+                <InsertarPersona insertar={this.insertar}/>
                 <TablaPersonas personas={personas} borrarPersona={this.borrarPersona}/>
             </div>
         );
     }
-    
+
     componentDidMount() {
         store.subscribe(()=>this.forceUpdate());
     }
