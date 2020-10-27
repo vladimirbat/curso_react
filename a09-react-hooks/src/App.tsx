edfbfbf61d5494a6e870c187a7aeb688307e668c
Vladimir Bataller <vbatalle@everis.com>
2020-10-27 16:48:24 +0100

    hooks

--- a09-react-hooks/src/App.tsx
+++ a09-react-hooks/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import { ComponenteConUseEfect } from './components/ComponenteConUseEffect';
 import { ComponenteConUseState } from './components/ComponenteConUseState';
 
 function App() {
   return (
-    <div className="App">
-      <ComponenteConUseState />
+    <div className="light">
+      <div className="container">
+        <div className="effect-example">
+            <div className="row">
+              <div className="col-3"></div>
+              <div className="col-6">
+                <h2>useEffect</h2>
+                <ComponenteConUseState />
+              </div>
+            </div>
+          </div>
+        <div className="effect-example">
+            <div className="row">
+              <div className="col-3"></div>
+              <div className="col-6">
+                <h2>useEffect</h2>
+                <ComponenteConUseEfect />
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
---
+++ a09-react-hooks/src/components/ComponenteConUseEffect.tsx
@@ -0,0 +1,36 @@
+import React, {FC, useRef, MouseEvent, useState, Fragment, useEffect} from 'react';
+
+export const ComponenteConUseEfect:FC<{}> = () =>{
+    const [dias, setDias] = useState<string[]>([]);
+    // Recibe una función que se ejecuta después del primer renderizado (componentDidMount),
+    // después de los siguientes renderizados (se puede eviar estas ejecuciones) (componentDidUpdate)
+
+    // useEffect(()=>{
+    //     console.log('useEffect');
+    //     if(dias.length===0) {
+    //         fetch('laborables.json')
+    //             .then((response:Response) => response.json())
+    //             .then(x => new Promise<any>((resolve) => setTimeout(() => resolve(x), 2000)))
+    //             .then(ds => setDias(ds));
+    //     }
+    // });
+    const textDias = dias.toString();
+    useEffect(()=>{
+        console.log('useEffect');
+        fetch('laborables.json')
+            .then((response:Response) => response.json())
+            .then(x => new Promise<any>((resolve) => setTimeout(() => resolve(x), 2000)))
+            .then(ds => setDias(ds));
+    },[textDias]);
+
+    if(dias.length){
+        return (<ul>
+            {dias.map((d,i) => {
+                return <li key={i}>{d}</li>
+            })}
+        </ul>);
+    }
+    return <div style={{textAlign:'center'}}>
+        <img src="loading.gif" alt="Loading..." style={{width:150, maxWidth:'90%'}}/>
+    </div>;
+}
